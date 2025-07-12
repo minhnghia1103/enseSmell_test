afb19bf02e583b7dbba9c62db32ae5b7f8130876
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2025-07-12 15:50:30 +0700

    sdasd

--- program/ensesmells/main.py
+++ program/ensesmells/main.py
@@ -138,11 +138,10 @@ if __name__ == "__main__":
     pos_weight_set = [
         torch.tensor(4.0, dtype=torch.float),
         torch.tensor(8.0, dtype=torch.float), 
-        torch.tensor(12.0, dtype=torch.float), 
-        torch.tensor(32.0, dtype=torch.float)
+        torch.tensor(12.0, dtype=torch.float)
     ]
 
-    kernel_size_set = [ 5]
+    kernel_size_set = [3]
     now = datetime.datetime.now()
 
     data_path = args.data_path
